4cb2474f51dc00e5b01796b0d55c0c4f27f604f6
shaud0w <shaudowrk@gmail.com>
2022-01-31 19:55:08 +0800

    reply-bot xd

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,14 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const telegraf_1 = require("telegraf");
+require("dotenv/config");
+const bot = new telegraf_1.Telegraf(process.env.BOT_TOKEN);
+bot.on('text', (ctx) => ctx.reply('👍'));
+bot.start((ctx) => ctx.reply('Hello'));
+bot.help((ctx) => ctx.reply('Help message'));
+// Launch bot after handlers are created
+bot.launch();
+// Shut down bot when you stop Node
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
 //# sourceMappingURL=app.js.map
