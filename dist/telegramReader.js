76b28cf936ecbdc6086ed6eb6867fb225c9d4430
shaud0w <shaudowrk@gmail.com>
2022-02-04 17:28:23 +0800

    reading channel works

--- dist/telegramReader.js
+++ dist/telegramReader.js
@@ -2,13 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const telegraf_1 = require("telegraf");
 require("dotenv/config");
-const bot = new telegraf_1.Telegraf(process.env.BOT_TOKEN);
-bot.on('text', (ctx) => ctx.reply('👍'));
-bot.start((ctx) => ctx.reply('Hello'));
-bot.help((ctx) => ctx.reply('Help message'));
+const token = process.env.TG_BOT_TOKEN;
+const bot = new telegraf_1.Telegraf(token);
+//const telegram: Telegram = new Telegram(token);
+//const chatId: string = process.env.CHAT_ID as string;
+bot.on("channel_post", (ctx) => ctx.reply("channel reply"));
 // Launch bot after handlers are created
 bot.launch();
 // Shut down bot when you stop Node
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
-//# sourceMappingURL=app.js.map
+//# sourceMappingURL=telegramReader.js.map
