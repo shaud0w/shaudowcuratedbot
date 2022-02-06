31f07953f8f0ebf4ebc262ac2c1b707fd2ac3f3a
shaud0w <shaudowrk@gmail.com>
2022-02-06 18:16:52 +0800

    forwarding bot done but only works with text

--- dist/bot.js
+++ dist/bot.js
@@ -7,28 +7,35 @@ const tgToken = process.env.TG_BOT_TOKEN;
 const discToken = process.env.DISCORD_BOT_TOKEN;
 // Initialize Telegram variables
 const tgBot = new telegraf_1.Telegraf(tgToken);
-//const telegram: Telegram = new Telegram(token);
-//const chatId: string = process.env.CHAT_ID as string;
 // Initialize Discord bot
 const discordBot = new discord_js_1.Client({ intents: [discord_js_1.Intents.FLAGS.GUILDS] });
-discordBot.on('ready', () => {
+// Make sure bot is ready
+discordBot.on("ready", () => {
     console.log("Shaudow Bot (Discord) is ready");
 });
 // Launch Discord bot
 discordBot.login(discToken);
 //Occurs from any channel bot is part of
 tgBot.on("channel_post", (ctx) => {
-    const channel = discordBot.channels.cache.get('935715549806227548');
-    console.log("ctx: " + ctx);
-    if (channel?.isText()) {
-        channel.send("sample");
+    const channel = discordBot.channels.cache.get("935715549806227548");
+    try {
+        let text = (0, telegraf_1.deunionize)(ctx.channelPost).text;
+        if (text === undefined) {
+            if (channel?.isText())
+                channel.send("Message was not text only." +
+                    "See telegram: https://t.me/shaudowcurated to see the message.");
+        }
+        if (channel?.isText() && text != undefined) {
+            channel.send(text);
+        }
     }
-    ;
+    catch (error) {
+        console.log(error);
+    }
 });
 // Launch TG bot
 tgBot.launch();
-//Send to channel
 // Shut down TG bot when you cleanly
-process.once('SIGINT', () => tgBot.stop('SIGINT'));
-process.once('SIGTERM', () => tgBot.stop('SIGTERM'));
+process.once("SIGINT", () => tgBot.stop("SIGINT"));
+process.once("SIGTERM", () => tgBot.stop("SIGTERM"));
 //# sourceMappingURL=bot.js.map
--- src/bot.ts
+++ src/bot.ts
@@ -1,36 +1,50 @@
-import { Context, Telegraf, Telegram } from 'telegraf';
-import { Update } from 'typegram';
-import { Client, Intents } from 'discord.js';
-import 'dotenv/config';
+import { Context, Telegraf, deunionize } from "telegraf";
+import { Message } from "typegram";
+
+import { Update } from "typegram";
+import { Client, Intents } from "discord.js";
+import "dotenv/config";
 
 const tgToken: string = process.env.TG_BOT_TOKEN as string;
 const discToken: string = process.env.DISCORD_BOT_TOKEN as string;
 
 // Initialize Telegram variables
 const tgBot: Telegraf<Context<Update>> = new Telegraf(tgToken);
-//const telegram: Telegram = new Telegram(token);
-//const chatId: string = process.env.CHAT_ID as string;
 
 // Initialize Discord bot
 const discordBot = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-discordBot.on('ready', () => {
-    console.log("Shaudow Bot (Discord) is ready")
-})
+// Make sure bot is ready
+discordBot.on("ready", () => {
+  console.log("Shaudow Bot (Discord) is ready");
+});
 
 // Launch Discord bot
 discordBot.login(discToken);
 
 //Occurs from any channel bot is part of
-tgBot.on("channel_post", (ctx: Context) => {
-    const channel = discordBot.channels.cache.get('935715549806227548');
-    console.log("ctx: " + ctx)
-    if (channel?.isText()) {channel.send("sample")};
-})
+tgBot.on("channel_post", (ctx) => {
+  const channel = discordBot.channels.cache.get("935715549806227548");
+  try {
+    let text = deunionize(ctx.channelPost).text;
+    if (text === undefined) {
+      if (channel?.isText())
+        channel.send(
+          "This message was not text only. " +
+            "See telegram: https://t.me/shaudowcurated to see the message."
+        );
+    }
+    if (channel?.isText() && text != undefined) {
+      channel.send(text!);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 // Launch TG bot
 tgBot.launch();
 
 // Shut down TG bot when you cleanly
-process.once('SIGINT', () => tgBot.stop('SIGINT'));
-process.once('SIGTERM', () => tgBot.stop('SIGTERM'));
+process.once("SIGINT", () => tgBot.stop("SIGINT"));
+process.once("SIGTERM", () => tgBot.stop("SIGTERM"));
